06d3427de6f819629288ea287ecb17918524a6d2
MounirSiraje <mounir.siraje@hotmail.com>
2023-12-18 22:48:29 +0100

    N

--- Website/Game/models.py
+++ Website/Game/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class CustomGame(models.Model):
+    DIFFICULTY_CHOICES = [
+        ('Impossible', 'impossible'),
+        ('Very Hard', 'very_hard'),
+        ('Hard', 'hard'),
+        ('Normal', 'normal'),
+        ('Easy', 'easy'),
+    ]
+    creator = models.CharField(max_length=255)
+    likes = models.IntegerField(default=0)
+    date_created = models.DateTimeField(auto_now_add=True)
+    map_title = models.CharField(max_length=100)
+    mobility = models.BooleanField(default=False)
+    difficulty = models.CharField(max_length = 20, choices = DIFFICULTY_CHOICES)
+    timer_status = models.BooleanField(default=False)
+    timer_duration = models.IntegerField(null=True, blank=True)
+    map_description = models.TextField(default="No description.", max_length=1000)
+    
+class Location(models.Model):
+    custom_game = models.ForeignKey(CustomGame, related_name='locations', on_delete=models.CASCADE)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
--- Website/User/views.py
+++ Website/User/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.utils import timezone
+from django.http import HttpResponseBadRequest
+from Game.models import CustomGame, Location
 
 def register(request):
     if request.method == "POST":
@@ -60,7 +63,80 @@ def log_out(request):
 
 def mygames(request):
     if request.user.is_authenticated:
-        fname = request.user.first_name
-        return render(request, "profile/mygames.html", {'fname' : fname})
+        return render(request, "profile/mygames.html")
     else:
-        return redirect('home') 
+        return redirect('home')
+    
+def create_game(request):
+    if request.method == 'POST':
+        creator_username = request.user.username if request.user.is_authenticated else None
+        mapTitle = request.POST.get('mapTitle', '').strip()
+        canMove = request.POST.get('canMove', '').strip()
+        scoreCalc = request.POST.get('scoreCalc', '').strip()
+        timerActive = request.POST.get('timerActive', '').strip()
+        timerDuration = request.POST.get('timerDuration', '').strip()
+        mapDescription = request.POST.get('mapDescription', '').strip()
+
+        # Validate required fields
+        if not all([mapTitle, canMove, scoreCalc, timerActive, mapDescription]):
+            return HttpResponseBadRequest("Please fill in all required fields.")
+
+        # If Timer is set to "Yes," ensure Timer Duration is filled
+        if timerActive == 'yes' and not timerDuration:
+            return HttpResponseBadRequest("Please fill the Timer Duration.")
+
+        # Validate timer duration range
+        if timerDuration and int(timerDuration) > 600:
+            return HttpResponseBadRequest("Timer duration cannot exceed 10 minutes.")
+
+        # Validate difficulty level
+        valid_difficulty_levels = ['Impossible', 'Very Hard', 'Hard', 'Normal', 'Easy']
+        if scoreCalc not in valid_difficulty_levels:
+            return HttpResponseBadRequest("Invalid difficulty level.")
+
+        print("Creating CustomGame instance...")
+        # Create CustomGame instance
+        custom_game = CustomGame.objects.create(
+            creator = creator_username,
+            map_title = mapTitle,
+            mobility = (canMove == 'yes'),
+            difficulty = scoreCalc,
+            timer_status = (timerActive == 'yes'),
+            timer_duration = int(timerDuration) if timerDuration else None,
+            map_description = mapDescription
+        )
+        print("CustomGame instance created.")
+        # Collect coordinates dynamically
+        latitudes = []
+        longitudes = []
+        location_index = 1
+
+        while True:
+            lat_key = f'lat{location_index}'
+            lon_key = f'long{location_index}'
+
+            if lat_key in request.POST and lon_key in request.POST:
+                latitudes.append(request.POST[lat_key])
+                longitudes.append(request.POST[lon_key])
+                location_index += 1
+            else:
+                break
+
+        # Validate coordinate values
+        for lat, lon in zip(latitudes, longitudes):
+            try:
+                lat, lon = float(lat), float(lon)
+                if not (-90 <= lat <= 90) or not (-180 <= lon <= 180):
+                    return HttpResponseBadRequest("Invalid coordinates.")
+            except ValueError:
+                return HttpResponseBadRequest("Invalid coordinate values.")
+
+        print(latitudes)
+        print(longitudes)
+        # Create Location instances
+        for lat, lon in zip(latitudes, longitudes):
+            Location.objects.create(custom_game=custom_game, latitude=lat, longitude=lon)
+        print("Location instances created.")
+        
+        return redirect ('mygames')
+    return render(request, "mygames.html")
