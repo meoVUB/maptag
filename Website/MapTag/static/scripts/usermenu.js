fc1b37f2d456fbeebd8537da334611cd8cf1ac50
Elliott.O <octave.elliott2@gmail.com>
2023-12-19 23:40:50 +0100

    z

--- Website/MapTag/static/scripts/usermenu.js
+++ Website/MapTag/static/scripts/usermenu.js
@@ -38,21 +38,3 @@ window.onclick = function (event) {
         }
     }
 }
-
-// Get the modal
-var modal3 = document.getElementById('reportbug-modal');
-var modal4 = document.getElementById('requestfeature-modal');
-
-console.log(modal3);
-console.log(modal4);
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    console.log("hey");
-    if (event.target == modal3) {
-        modal3.style.display = "none";
-    }
-    if (event.target == modal4) {
-        modal4.style.display = "none";
-    }
-}
