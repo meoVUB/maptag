30ac8ab8e70605a1ce132c49e7bfbe341723bbf7
Elliott.O <octave.elliott2@gmail.com>
2023-12-19 21:37:01 +0100

    put it in base js

--- Website/MapTag/static/scripts/usermenu.js
+++ Website/MapTag/static/scripts/usermenu.js
@@ -1,11 +1,32 @@
 /* When the user clicks on the button, 
             toggle between hiding and showing the dropdown content */
+
+            
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
+var modals;
+
+window.onload = function() {
+// Get all elements with the class 'modal'
+modals = document.querySelectorAll('.modal');
+
+console.log("Modals:", modals);
+
+// Log each modal for verification
+modals.forEach(function(modal, index) {
+    console.log("Modal " + (index + 1) + ":", modal);
+});
+}
+
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (event) {
+    modals.forEach(function(modal) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
     if (!event.target.matches('.dropbtn')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
