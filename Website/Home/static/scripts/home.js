0d57d67f3cb667442510715f09bbda1dd1319ac5
Elliott.O <octave.elliott2@gmail.com>
2023-12-19 21:22:47 +0100

    fixed modal dissapearing when clicking somewhere else on the window

--- Website/Home/static/scripts/home.js
+++ Website/Home/static/scripts/home.js
@@ -1,27 +1,22 @@
-// Get the modal
-var modal1 = document.getElementById('register-modal');
-var modal2 = document.getElementById('login-modal');
-var modal3 = document.getElementById('reportbug-modal');
-var modal4 = document.getElementById('requestfeature-modal');
 
-console.log(modal1);
-console.log(modal2);
-console.log(modal3);
-console.log(modal4);
+var modals;
 
-// When the user clicks anywhere outside of the modal, close it
+window.onload = function() {
+// Get all elements with the class 'modal'
+modals = document.querySelectorAll('.modal');
+
+console.log("Modals:", modals);
+
+// Log each modal for verification
+modals.forEach(function(modal, index) {
+    console.log("Modal " + (index + 1) + ":", modal);
+});
+}
+// When the user clicks anywhere outside of a modal, close the relevant modal
 window.onclick = function(event) {
-    console.log("hey");
-    if (event.target == modal1) {
-        modal1.style.display = "none";
-    }
-    if (event.target == modal2) {
-        modal2.style.display = "none";
-    }
-    if (event.target == modal3) {
-        modal3.style.display = "none";
-    }
-    if (event.target == modal4) {
-        modal4.style.display = "none";
-    }
+    modals.forEach(function(modal) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
 }
