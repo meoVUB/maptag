1a4ec56590f5b5dbf0b7839a8e16d6cadfc196f9
MounirSiraje <mounir.siraje@hotmail.com>
2023-12-19 17:56:17 +0100

    a

--- Website/Game/views.py
+++ Website/Game/views.py
@@ -14,7 +14,6 @@ def get_locations_for_game(request, game_id):
     custom_game = get_object_or_404(CustomGame, id=game_id)
     locations = Location.objects.filter(custom_game=custom_game)
     serialized_locations = serializers.serialize('json', locations)
-    print(json.loads(serialized_locations))
     return JsonResponse({'locations': serialized_locations}, safe=False)
 
 # Create your views here.
