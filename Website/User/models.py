9cf70871f2b803a90b5e2415145b35aa63856323
Elliott.O <octave.elliott2@gmail.com>
2023-12-20 18:27:39 +0100

    Fixed request, report bug

--- Website/User/models.py
+++ Website/User/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Report(models.Model):
+    description = models.TextField()
+
+class Feature(models.Model):
+    description = models.TextField()
